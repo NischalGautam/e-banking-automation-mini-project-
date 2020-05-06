78ba4cf01edfadefbd23ec5e8362f73363668bd9
Nischal <nischal.gautam08@yahoo.com>
2020-05-06 13:30:10 -0500

    Created new pageobject delete customer

---
+++ src/test/java/com/internetBanking/pageObjects/DeleteCustomer.java
@@ -0,0 +1,51 @@
+package com.internetBanking.pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+public class DeleteCustomer {
+
+	WebDriver driver;
+
+	public DeleteCustomer(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(how = How.LINK_TEXT, using = "Edit Customer")
+	@CacheLookup
+	WebElement editCusBtn;
+
+	@FindBy(how = How.NAME, using = "cusid")
+	@CacheLookup
+	WebElement idInput;
+
+	@FindBy(how = How.NAME, using = "AccSubmit")
+	@CacheLookup
+	WebElement submitBtn;
+
+	@FindBy(how = How.LINK_TEXT, using = "Continue")
+	@CacheLookup
+	WebElement homeLink;
+
+	public void clickEditCusBtn() {
+		editCusBtn.click();
+	}
+
+	public void cusIdInput(String id) {
+		idInput.sendKeys(id);
+	}
+
+	public void submitBtn() {
+		submitBtn.click();
+	}
+
+	public void homeLink() {
+		homeLink.click();
+	}
+
+}
